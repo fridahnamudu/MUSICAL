d02f2359a693abe6a8b805491c82aa82f795411e
fridahnamudu <fridah.namudu@gmail.com>
2022-01-05 20:47:42 +0300

    add

---
+++ gospel.php
@@ -0,0 +1,81 @@
+<?php
+include_once 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+    <tittle></tittle>
+         <script src="js/player6.js"></script>
+              
+                <a href="javascript:void();"class="" dataswitch="0">
+                <a href="#;"class="" dataswitch="2">
+                <a href="#;"class="" dataswitch="3">
+                <a href="#;"class="" dataswitch="4">
+                <a href="#;"class="" dataswitch="5">
+                <a href="#;"class="" dataswitch="6">
+                <a href="#;"class="" dataswitch="7">
+                <a href="#;"class="" dataswitch="8">
+                <a href="#;"class="" dataswitch="9">
+                <a href="#;"class="" dataswitch="10">
+                <a href="#;"class="" dataswitch="11">
+                <a href="#;"class="" dataswitch="12">
+                <a href="#;"class="" dataswitch="13">
+                <a href="#;"class="" dataswitch="14">
+                <a href="#;"class="" dataswitch="15">
+                <a href="#;"class="" dataswitch="16">
+                <a href="#;"class="" dataswitch="17">
+                <a href="#;"class="" dataswitch="18">
+                <a href="#;"class="" dataswitch="19">
+                <a href="#;"class="" dataswitch="20">
+                <a href="#;"class="" dataswitch="21">
+                <a href="#;"class="" dataswitch="22">
+                <a href="#;"class="" dataswitch="23">
+                <a href="#;"class="" dataswitch="24">
+                <a href="#;"class="" dataswitch="25">
+                <a href="#;"class="" dataswitch="26">
+                <a href="#;"class="" dataswitch="27">
+                <a href="#;"class="" dataswitch="28">
+                <a href="#;"class="" dataswitch="29">  
+                <a href="#;"class="" dataswitch="30">
+                <a href="#;"class="" dataswitch="31">
+                <a href="#;"class="" dataswitch="32">
+                <a href="#;"class="" dataswitch="33">
+                <a href="#;"class="" dataswitch="34">
+                <a href="#;"class="" dataswitch="35">
+                <a href="#;"class="" dataswitch="36">
+                <a href="#;"class="" dataswitch="37">
+                <a href="#;"class="" dataswitch="38">
+                <a href="#;"class="" dataswitch="39">
+                <a href="#;"class="" dataswitch="40">
+                <a href="#;"class="" dataswitch="41">
+                <a href="#;"class="" dataswitch="42">
+                <a href="#;"class="" dataswitch="43">
+                <a href="#;"class="" dataswitch="44">
+                <a href="#;"class="" dataswitch="45">
+                <a href="#;"class="" dataswitch="46">
+                <a href="#;"class="" dataswitch="47">
+                <a href="#;"class="" dataswitch="48">
+                <a href="#;"class="" dataswitch="49">
+                <a href="#;"class="" dataswitch="50">
+                <a href="#;"class="" dataswitch="51">
+                <a href="#;"class="" dataswitch="52">
+                <a href="#;"class="" dataswitch="53">
+                <a href="#;"class="" dataswitch="54">
+                <a href="#;"class="" dataswitch="55">
+                <a href="#;"class="" dataswitch="56">
+                <a href="#;"class="" dataswitch="57">
+                <a href="#;"class="" dataswitch="58">
+                <a href="#;"class="" dataswitch="59">
+                <a href="#;"class="" dataswitch="60">
+                <a href="#;"class="" dataswitch="61">
+                </a>
+              
+            
+          
+
+         <div id="player6" class="aplayer">
+            
+            Loading...
+          </div>
+          <script src="js/player6.js"></script>
+    </body>
+    </html>
