8de47544a0cc5ba1ebc5880b25861d19652615cb
fridahnamudu <fridah.namudu@gmail.com>
2022-01-05 20:50:35 +0300

    add

---
+++ slide.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <tittle></tittle>
+    <head> 
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" href="">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">     
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="css/styles.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css.map">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Niconne&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css">
+        <link rel="stylesheet" href="APlayer.min.css">
+        <link href="css/styles.css" rel="stylesheet" type="text/css">
+        <link href="css/songs.css" rel="stylesheet" type="text/css">
+        <link href="css/play.css" rel="stylesheet" type="text/css">
+
+
+
+        <script
+        src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+        crossorigin="anonymous"></script>
+        <script src="js/script.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js.map"></script>
+    </head>
+    <div id="carousel" class="carousel slide" data-ride="carousel" data-interval="">
+     <div class="carousel-inner">
+         <div class="carousel-item active">
+            <img src="" alt="">
+        </div>
+     </div>
+    </div>
+
+</html>
