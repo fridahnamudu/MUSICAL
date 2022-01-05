65ac0c4db66d2326237d1364307f3a88fa5144e5
fridahnamudu <fridah.namudu@gmail.com>
2022-01-05 20:49:06 +0300

    add

---
+++ pop.php
@@ -0,0 +1,100 @@
+<?php
+include_once 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+         <script src="js/player2.js"></script>
+              <h2><em>POP  MUSIC</em></h2>
+        
+            <a href="javascript:void();"class="" dataswitch="0">
+             
+         </div> 
+         <div class="main">
+          <div class="container">
+            <div id="aplayer"></div>
+            <div class="row">
+             
+              <div="div1">linear
+              
+              </div>
+              </div>
+              <img src="/images/lipa.jpeg"></a>
+              
+              <img src="/images/halsey.jpeg"></a>
+              <img src="/images/chord.jpeg" ></a>
+              <img src="/images/nas.jpeg"></a>
+              <img src="/images/lizzo.jpeg"></a>
+              <img src="/images/marsh.jpeg"></a>
+              <img src="/images/tate.jpeg"></a>
+              <img src="/images/tones.jpeg"></a>
+              <img src="/images/roddy.jpeg"></a>
+              <img src="/images/Time.jpeg"></a>
+              <img src="/images/zedd.jpeg"></a>
+            </div>
+  </div>
+</div>
+         <a href="javascript:void();"class="" dataswitch="0">
+         <a href="#;"class="" dataswitch="1">
+         <a href="#;"class="" dataswitch="2">
+         <a href="#;"class="" dataswitch="3">
+         <a href="#;"class="" dataswitch="4">
+         <a href="#;"class="" dataswitch="5">
+         <a href="#;"class="" dataswitch="6">
+         <a href="#;"class="" dataswitch="7">
+         <a href="#;"class="" dataswitch="8">
+         <a href="#;"class="" dataswitch="9">
+         <a href="#;"class="" dataswitch="10">
+         <a href="#;"class="" dataswitch="11">
+         <a href="#;"class="" dataswitch="12">
+         <a href="#;"class="" dataswitch="13">
+         <a href="#;"class="" dataswitch="14">
+         <a href="#;"class="" dataswitch="15">
+         <a href="#;"class="" dataswitch="16">
+         <a href="#;"class="" dataswitch="17">
+         <a href="#;"class="" dataswitch="18">
+         <a href="#;"class="" dataswitch="19">
+         <a href="#;"class="" dataswitch="20">
+         <a href="#;"class="" dataswitch="21">
+         <a href="#;"class="" dataswitch="22">
+         <a href="#;"class="" dataswitch="23">
+         <a href="#;"class="" dataswitch="24">
+         <a href="#;"class="" dataswitch="25">
+         <a href="#;"class="" dataswitch="26">
+         <a href="#;"class="" dataswitch="27">
+         <a href="#;"class="" dataswitch="28">
+         <a href="#;"class="" dataswitch="29">  
+         <a href="#;"class="" dataswitch="30">
+         <a href="#;"class="" dataswitch="31">
+         <a href="#;"class="" dataswitch="32">
+         <a href="#;"class="" dataswitch="33">
+         <a href="#;"class="" dataswitch="34">
+         <a href="#;"class="" dataswitch="35">
+         <a href="#;"class="" dataswitch="36">
+         <a href="#;"class="" dataswitch="37">
+         <a href="#;"class="" dataswitch="38">
+         <a href="#;"class="" dataswitch="39">
+         <a href="#;"class="" dataswitch="40">
+         <a href="#;"class="" dataswitch="41">
+         <a href="#;"class="" dataswitch="42">
+         <a href="#;"class="" dataswitch="43">
+         <a href="#;"class="" dataswitch="44">
+         <a href="#;"class="" dataswitch="45">
+         <a href="#;"class="" dataswitch="46">
+         <a href="#;"class="" dataswitch="47">
+         <a href="#;"class="" dataswitch="48">
+         <a href="#;"class="" dataswitch="49">
+         <a href="#;"class="" dataswitch="50">
+         <a href="#;"class="" dataswitch="51">
+         <a href="#;"class="" dataswitch="52"></a> 
+       
+    
+     
+         <div id="player2" class="aplayer">
+          <div style="padding: 10px; font-size: 10px; text-align: center;"></div>
+
+            
+            Loading...
+          </div>
+          <script src="js/player2.js"></script>
+    </body>
+    </html>
