742dca45e991b190c25935e47586354024e12e5a
fridahnamudu <fridah.namudu@gmail.com>
2022-01-05 20:51:10 +0300

    covers.php

---
+++ covers.php
@@ -0,0 +1,18 @@
+<?php 
+include_once 'header.php'
+?>
+<!DOCTYPE html>
+<html>
+    <tittle></tittle>
+         <script src="js/player15.js"></script>
+     
+            <a href="javascript:void();"class="" dataswitch="0">
+              
+
+         <div id="player15" class="aplayer">
+            
+            Loading...
+          </div>
+          <script src="js/player15.js"></script>
+    </body>
+    </html>
