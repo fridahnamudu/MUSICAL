18b42b10a51694085b4dca28b7dfd07af609347e
fridahnamudu <fridah.namudu@gmail.com>
2022-01-05 20:48:56 +0300

    add

---
+++ playlists.php
@@ -0,0 +1,52 @@
+<?php
+include_once 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+           <h2> PLAYLISTS</h2>
+        <p> View and listen to your preffered playlist over here, you are also allowed to like &#128077;</p>
+        <p>We have playlists of different categories of music ie:
+          <div class="typesongs">
+            
+              <li><a  href="pop.php">POP</a></li>
+              <li><a href="blues.php">BLUES</a></li>
+              <li><a href="country.php">COUNTRY SONGS</a></li>
+              <li><a href="edm.php">EDM</a></li>
+              <li><a href="reggae.php">REGGAE</a></li>
+              <li><a href="rap.php">HIP-HOP/RAP</a></li>
+              <li><a  href="rnb.php">R&B</a></li>
+              <li><a href="gospel.php">GOSPEL</a></li>
+              <li><a href="holiday.php">HOLIDAY</a></li>
+              <li><a href="covers.php">COVERS</a></li>
+              
+            
+          </div>
+
+            <div class=".songs">
+            <ul>
+                <li>R&B</li>
+                <li> &#128150;POP MUSIC </li>
+                <li>AFRICAN MUSIC (gengetone , naija, Ugandan music and  South Africa)</li>
+                <li> &#127932;SOUL MUSIC</li>
+                <li>  &#127927; SOUL MUSIC</li>
+                <li> &#128591;GOSPEL MUSIC</li>
+                <li> &#128118;KIDS SONGS</li>
+            </ul>
+            </div>
+        </p>
+        <p> We also promote all the upcomming artists who want to partner with us.</p>
+       
+        <div id="aplayer"> 
+       
+<div id="player1" class="aplayer">
+  <!-- <button onclick="APlayer"></button> -->
+  Loading...
+</div>
+<script src="js/player.js"></script>
+     
+  </body>
+        
+</html>
+
+        
+       
